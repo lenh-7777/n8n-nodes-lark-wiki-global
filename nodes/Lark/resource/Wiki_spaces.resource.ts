1ddfeac015feaf0b7e3f7b0a7890753869f6a8a2
lenh <lenh@mobio.io>
2026-03-04 15:30:41 +0700

    update wiki_space

---
+++ nodes/Lark/resource/Wiki_spaces.resource.ts
@@ -0,0 +1,8 @@
+import { ResourceType } from '../../help/type/enums';
+import { ResourceOptions } from '../../help/type/IResource';
+
+export default {
+    name: 'Wiki Spaces',
+    value: ResourceType.Wiki,
+    order: 140,
+} as ResourceOptions;
